370b25492e5bb7e48e40a7ee51ef7863ddedbc56
Paola Julie <paolapicapedra@gmail.com>
2024-07-05 14:56:06 -0300

    adicionando uma view, registerPage

--- Endividei/src/index.js
+++ Endividei/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import LoginPage from './views/LoginPage/LoginPage';
+import RegisterPage from './views/RegisterPage/RegisterPage';
 
 export default function App() {
   return (
     <>
-      <LoginPage />
+      <RegisterPage />
     </>
   );
 } 
