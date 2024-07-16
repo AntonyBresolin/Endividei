d112b2b50376fe7a65d8f349018407aa5fe67f86
Paola Julie Santos <126926406+paolajulie@users.noreply.github.com>
2024-07-16 12:12:34 -0300

    Frontend de tela de boas vindas feita (#4)

--- Endividei/App.js
+++ Endividei/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Navigation from './src/routes/Navigation';
-import MovementsPage from './src/views/MovementsPage/MovementsPage';
-
 export default function App() {
   return (
     <>
